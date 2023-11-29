28fbd33af14ffc19d7f0baec81c11386e16e1f6f
michael-svoboda <michael.svoboda1@ucalgary.ca>
2023-11-29 12:34:56 -0700

    Completed the solve method for DarcyFlowSS. Tested it solving for every variable.

--- latex_edited_string.py
+++ latex_edited_string.py
@@ -1,10 +1,11 @@
+
 from sympy import symbols, Eq, solve, pi, log, latex
 
 # Define symbols
-Q, k, h, r1, r2, delta_P = symbols('Q k h r1 r2 delta_P')
+q, k, h, r1, r2, delta_P = symbols('q k h r1 r2 delta_P')
 
 # Define Darcy's law equation symbolically
-darcys_law_radial = Eq(Q, -2 * pi * k * h * delta_P / log(r2 / r1))
+darcys_law_radial = Eq(q, -2 * pi * k * h * delta_P / log(r2 / r1))
 
 # Define numerical values for other variables
 k_value = 1.5  # replace with your value
@@ -13,11 +14,17 @@ r1_value = 2.0  # replace with your value
 r2_value = 4.0  # replace with your value
 delta_P_value = 100.0  # replace with your value
 
-# Rearrange the equation
-rearranged_equation = solve(darcys_law_radial, Q)[0]
+# Rearrange the equation and solve for q
+rearranged_equation = solve(darcys_law_radial, q)[0]
+
+# Substitute numerical values into the solution
+solution_with_values = rearranged_equation.subs({k: k_value, h: h_value, r1: r1_value, r2: r2_value, delta_P: delta_P_value})
 
 # Get the LaTeX string of the rearranged equation
 rearranged_latex = latex(rearranged_equation)
+
+# Get the LaTeX string of the solution with substituted numerical values
+solution_latex_with_values = solution_with_values
 
 # Substitute numerical values into the LaTeX string
 rearranged_latex_with_values = rearranged_latex.replace(latex(k), str(k_value)) \
@@ -31,3 +38,6 @@ print(f"Rearranged Equation: {rearranged_latex}")
 
 # Print the rearranged equation with substituted numerical values in LaTeX
 print(f"Rearranged Equation with Values: {rearranged_latex_with_values}")
+
+# Print the solution to the equation with substituted numerical values in LaTeX
+print(f"Solution with Values: {solution_latex_with_values}")
--- util/reservoir/DarcyFlowSS.py
+++ util/reservoir/DarcyFlowSS.py
@@ -1,69 +1,117 @@
-from sympy import symbols, pi, log, solve
+from sympy import symbols, Eq, solve, pi, log
 
 class DarcysLawBase:
+
+    q, k, h, rw, re, Pwf, Pe, mu = symbols('q k h rw re Pwf Pe mu')
+
     def __init__(self, **kwargs):
+        self.flow_rate = kwargs.get('q', None)
         self.permeability = kwargs.get('k', None)
         self.thickness = kwargs.get('h', None)
         self.inner_radius = kwargs.get('rw', None)
         self.outer_radius = kwargs.get('re', None)
         self.bottomhole_pressure = kwargs.get('Pwf', None)
         self.edge_pressure = kwargs.get('Pe', None)
+        self.viscosity = kwargs.get('mu', None)
 
-        self.check_variables()
+        #self.check_variables()
 
+    def get_variable(self, variable):
+        if hasattr(self, variable):
+            return getattr(self, variable)
+        else:
+            raise ValueError(f"Variable '{variable}' not found.")
+
+    def get_variables(self):
+        # Get all variables initialized with numbers
+        variables_with_values = {symbol: getattr(self, var) for symbol, var in self.get_variable_symbols().items() if isinstance(getattr(self, var), (int, float))}
+        return variables_with_values
+
+    def get_variable_symbols(self):
+        # Define symbols and associate them with variable names
+        q, k, h, rw, re, Pwf, Pe, mu = symbols('q k h rw re Pwf Pe mu')
+        return {q: 'flow_rate', k: 'permeability', h: 'thickness', rw: 'inner_radius', re: 'outer_radius', Pwf: 'bottomhole_pressure', Pe: 'edge_pressure', mu: 'viscosity'}
+    
     def check_variables(self):
-        undefined_vars = [var for var in ['permeability', 'thickness', 'inner_radius', 'outer_radius', 'bottomhole_pressure', 'edge_pressure'] if getattr(self, var) is None]
+
+        undefined_vars = [var for var in ['flow_rate', 'permeability', 'thickness', 'inner_radius', 'outer_radius', 'bottomhole_pressure', 'edge_pressure', 'viscosity'] if getattr(self, var) is None]
+        
         if undefined_vars:
             raise ValueError(f"The following variables are not defined: {', '.join(undefined_vars)}")
 
     def solve(self, variable):
-        if variable not in ['permeability', 'thickness', 'inner_radius', 'outer_radius', 'bottomhole_pressure', 'edge_pressure']:
-            raise ValueError(f"Invalid variable '{variable}'")
 
-        equation = self.get_equation(variable)
-        return solve(equation, variable)[0]
+        equation = self.get_equation()
+        rearranged_equation = solve(equation, self.get_variable(variable))[0]
 
-    def get_equation(self, variable):
+        self.get_variables().keys()
+
+        # Substitute numerical values into the solution
+        values = self.get_variables()
+        print("VALUES: ", values)
+        solution_with_values = rearranged_equation.subs(values)
+
+        # Round the solution to two decimal places
+        #rounded_solution = round(solution_with_values, 4)
+
+
+        return solution_with_values
+
+    def get_equation(self):
         raise NotImplementedError("Subclasses must implement the get_equation method.")
 
 class DarcysLawSI(DarcysLawBase):
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-    def calculate_flow_rate(self):
-        numerator = -2 * pi * self.permeability * self.thickness * (self.edge_pressure - self.bottomhole_pressure)
-        denominator = log(self.outer_radius / self.inner_radius)
-        return numerator / denominator
+    def get_equation(self):
+        # Define Darcy's law equation symbolically
+        darcys_law_radial = Eq(self.q, (-2 * pi * self.k * self.h * (self.Pe - self.Pwf)) / ( log(self.re / self.rw)))
+        return darcys_law_radial
 
-    def get_equation(self, variable):
-        # Define generic equation for Darcy's Law in SI units
-        equation_dict = {
-            'permeability': self.calculate_flow_rate() * log(self.outer_radius / self.inner_radius) / (-2 * pi * self.thickness * self.delta_P),
-            'thickness': -self.calculate_flow_rate() * log(self.outer_radius / self.inner_radius) / (2 * pi * self.permeability * self.delta_P),
-            'inner_radius': self.inner_radius,
-            'outer_radius': self.outer_radius,
-            'delta_P': -self.calculate_flow_rate() * log(self.outer_radius / self.inner_radius) / (2 * pi * self.permeability * self.thickness)
-        }
-        return equation_dict[variable]
+class DarcysLawOF(DarcysLawBase):
 
-class DarcysLawOilfield(DarcysLawBase):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-    def calculate_flow_rate(self):
-        # Implement calculation with oilfield units
-        # Adjust units as needed
-        numerator = -2 * pi * self.permeability * self.thickness * self.delta_P
-        denominator = log(self.outer_radius / self.inner_radius)
-        return numerator / denominator
+    def get_equation(self):
+        # Define Darcy's law equation symbolically
+        darcys_law_radial = Eq(self.q, (self.k * self.h * (self.Pe - self.Pwf)) / (self.mu * 141.22 * log(self.re / self.rw)))
+        return darcys_law_radial
 
-    def get_equation(self, variable):
-        # Define generic equation for Darcy's Law in oilfield units
-        equation_dict = {
-            'permeability': self.calculate_flow_rate() * log(self.outer_radius / self.inner_radius) / (-2 * pi * self.thickness * self.delta_P),
-            'thickness': -self.calculate_flow_rate() * log(self.outer_radius / self.inner_radius) / (2 * pi * self.permeability * self.delta_P),
-            'inner_radius': self.inner_radius,
-            'outer_radius': self.outer_radius,
-            'delta_P': -self.calculate_flow_rate() * log(self.outer_radius / self.inner_radius) / (2 * pi * self.permeability * self.thickness)
-        }
-        return equation_dict[variable]
+
+
+# Your class definition here...
+
+# Sample implementation to print the result of get_variables
+if __name__ == "__main__":
+    # Create an instance of your class with some values
+    q=137.5344
+    k=1
+    h=131.2
+    rw=0.328
+    re=1640
+    Pwf=2320
+    Pe=2900
+    mu=0.46
+    darcy_instance = DarcysLawOF(q=q, k=k, h=h, rw=rw, re=re, Pwf=Pwf, Pe=Pe)
+
+    # List of variables to solve for
+    variables_to_solve = ['q', 'k', 'h', 'rw', 're', 'Pwf', 'Pe']
+
+    # Loop through each variable, solve, and print the result
+    for variable in variables_to_solve:
+        solution = darcy_instance.solve(variable)
+        print(f"Solving for {variable}: {solution}")
+
+
+
+'''# Example usage
+si_darcy = DarcysLawSI(k=1.5, h=0.2, rw=2.0, re=4.0, Pwf=100.0, Pe=50.0)
+solution_permeability = si_darcy.solve('permeability')
+print(f"Permeability solution: {solution_permeability}")
+
+oilfield_darcy = DarcysLawOilfield(k=2.0, h=0.3, rw=1.5, re=3.0, Pwf=120.0, Pe=60.0)
+solution_thickness = oilfield_darcy.solve('thickness')
+print(f"Thickness solution: {solution_thickness}")'''
